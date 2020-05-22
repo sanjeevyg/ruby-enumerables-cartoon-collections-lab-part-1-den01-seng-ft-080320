991cc2a86f4b5e63642b44eb9a02e558c2087358
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-22 01:55:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each {|element|
-  p "((array[array.index(element)]) + 1))" + ". " + element + "!"
+   array.each {|element|
+   p "#{array.index(element) + 1}. #{element}!"
   }
 end
